d6bb110ba7641f6a2f9ad59c63a76d17316590de
Trystan Nguyen <trystan.t.nguyen@gmail.com>
2025-03-06 16:45:19 -0800

    Add obstacles to long sections of no activity. Add Checkpoints. Change up positioning

--- Assets/Scripts/Respawner.cs
+++ Assets/Scripts/Respawner.cs
@@ -5,9 +5,21 @@ public class Respawner : MonoBehaviour
     public GameObject Player;
     public GameObject Respawn;
 
+    private CheckPoint mark;
+
     private void RespawnPlayer()
     {
         Player.transform.position = Respawn.transform.position;
-        Player.transform.rotation = Respawn.transform.rotation;
+        //Player.transform.rotation = Respawn.transform.rotation;
+    }
+
+    private void SetCheckPoint(CheckPoint newCheckPoint)
+    {
+        if(mark)
+            mark.BroadcastMessage("DisableCheckPoint");
+        mark = newCheckPoint;
+        Respawn.transform.position = mark.transform.position;
+        //Respawn.transform.rotation = mark.transform.rotation;
+        Debug.Log("Set CheckPoint");
     }
 }
